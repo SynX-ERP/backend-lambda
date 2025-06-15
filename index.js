5b84e3968c5d24e7b2f2fcd901fc0fc24d43681e
Lucas Ievenes Ratti <61846972+Ievenes@users.noreply.github.com>
2025-06-15 20:55:36 -0300

    apidoc

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Rotas
 const usuariosRoutes = require('./routes/usuariosRoutes');
